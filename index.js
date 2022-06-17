74fff903669e5995146d601809c4962ca467c287
Peter Ndumia <ndegwandumia@gmail.com>
2022-06-18 01:32:25 +0300

    commit tree 46b6e92788f5a535b5d23ba24fadbd0262b8a2f4

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+function submitData(userName, userEmail){
+
+    const userDets = {name: userName, email: userEmail}
+    const myFetch = fetch ('http://localhost:3000/users', {
+        method: 'POST',
+        headers:{
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+    },
+    body: JSON.stringify(userDets),  
+    })
+
+   .then(resp => resp.json())
+   .then(user => document.body.textContent = user.id)
+   .catch(e => document.body.textContent = e.message)
+    return myFetch
+}
